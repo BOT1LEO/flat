b48b4898894e5aba2a1c062f0f18de704a045094
BOT1LEO <66963910+BOT1LEO@users.noreply.github.com>
2022-07-28 23:25:17 +0100

    Basic main menu navigation system
    Added main menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,75 @@
+
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(const MyApp());
+void main() {
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: MainMenu(),
+  ));
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({ Key? key }) : super(key: key);
+class MainMenu extends StatefulWidget {
+  const MainMenu({ Key? key }) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<MainMenu> createState() => _MainMenuState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MainMenuState extends State<MainMenu> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("StatTracker"),
+        backgroundColor: Colors.green,
+      ),
+      body: ListView(
+        children: [
+          ElevatedButton(
+            child: const Text("Pushup"),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => Stats(value: "Pushups")));
+            } 
+            ),
+        ],
+      ),
     );
   }
 }
 
+class Stats extends StatefulWidget {
+  String value = "";
+  Stats({required this.value});
+  @override
+  State<Stats> createState() => _StatsState(value: value);
+}
+
+class _StatsState extends State<Stats> {
+  String value = "";
+  _StatsState({required this.value});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(value),
+        backgroundColor: Colors.green,
+      ),
+      body:BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Best"
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.graphic_eq),
+            label: "Graph"
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings"
+          ),
+        ],
+      )
+    );
+  }
+}
